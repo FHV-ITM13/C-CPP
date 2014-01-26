0fa57d0a20b199b4e1791ad6a55f79cb0c1ea7e6
sla89 <skullsplitter@die-optimisten.net>
2014-01-26 16:15:00 +0100

    added another union test in C

--- UnionTest/src/UnionTest.c
+++ UnionTest/src/UnionTest.c
@@ -24,8 +24,28 @@ union test2 {
    double c;
 };
 
+typedef unsigned char byte;
+
+typedef union {
+	int raw;
+	struct {
+		byte r;
+		byte g;
+		byte b;
+	};
+} color;
+
 int main(void) {
-   printf("struct benoetigt %d Bytes\n", sizeof(struct test1));
-   printf("Union  benoetigt %d Bytes\n", sizeof(union test2));
+   printf("struct test1 benoetigt %d Bytes\n", sizeof(struct test1));
+   printf("Union test2  benoetigt %d Bytes\n", sizeof(union test2));
+   printf("Union Color benoetigt %d Bytes\n", sizeof(color));
+
+   color c;
+   c.r = 12;
+   c.g = 5;
+   c.b = 255;
+   printf("Color: r: %d, g: %d, b: %d\n", c.r, c.g, c.b);
+   printf("Color: raw: %x\n", c.raw);
+
    return EXIT_SUCCESS;
 }
